fb1b32f405efb2be8c9120ebb8c9f9281082933b
Vikas <vikasjagali0@gmail.com>
2024-10-02 23:55:15 +0530

    review comments implemented

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -3,7 +3,12 @@ package com.qa.opencart.pages;
 public class DemoPage {
   int x = 10;
   
+  //updated page 
+  
+  
+  
   void textMe() {
 	  System.out.println("dont tets me");
+	  //updated method
   }
 }
